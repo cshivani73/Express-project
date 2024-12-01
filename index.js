e19a81b5ce62716a21f77f8a7a105a68b877644b
shivani <shivanichouhan697@gmail.com>
2024-12-01 11:46:04 +0530

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/api' , (req, res)=>{
-    console.log("hello User!");
+    console.log("hello new user!");
 })
 app.use('/api/user',require('./router/router'));
 
